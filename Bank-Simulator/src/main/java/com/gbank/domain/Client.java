444f6aa6fa9ecb5c575737bf5333266b00fcac24
Bishwo Gurung <bishwo.gurung3@gmail.com>
2018-08-16 20:14:52 -0400

    intial setup

---
+++ Bank-Simulator/src/main/java/com/gbank/domain/Client.java
@@ -0,0 +1,11 @@
+package com.gbank.domain;
+
+public class Client {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+}
